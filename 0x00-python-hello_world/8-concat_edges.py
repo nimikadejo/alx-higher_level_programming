90bc76b3500544642b01353538c26f55bf8617af
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-22 16:58:03 -0700

    more print string manipulation

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[-67:-39] + str[-56:] + str[:6]
+print(str)
