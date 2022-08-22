0ffae7742964752d59734f97deeefdf49f4083e5
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-22 15:20:41 -0700

    fix requirement error

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 number = 98
-number = int(number)
-print("{} Battery street" .format(number))
+#number = int(number)
+#print("{} Battery street" .format(number))
+print(f"{number:d} Battery street")
