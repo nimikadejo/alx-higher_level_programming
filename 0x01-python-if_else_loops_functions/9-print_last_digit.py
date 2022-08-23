936a3ad93f0007394842f5538816bf1093168812
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-23 16:40:45 -0700

    return value in function

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,5 +3,6 @@ def print_last_digit(number):
     if (number > 0):
         last = number % 10
     else:
-        last = ((number * -1) % 10)
-    print("{}".format(last), end='')
+        last = (number * -1) % 10
+    print("{}".format(last), end='i')
+    return last
