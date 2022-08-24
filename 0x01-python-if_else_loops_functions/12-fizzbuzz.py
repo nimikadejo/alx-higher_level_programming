69f9ac5214961354b91404d0667b9eb6eb5e6df6
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-24 14:26:52 -0700

    fizz error

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,8 +4,8 @@ def fizzbuzz():
         if ((i % 3) == 0):
             print("Fizz ")
         elif ((i % 5) == 0):
-            print("Buzz ")
+            print("Buzz ", end="")
         elif ((i % 3) == 0) and ((i % 5) == 0):
-            print("FizzBuzz ")
+            print("FizzBuzz ", end="")
         else:
-            print("{} ".format(i))
+            print("{} ".format(i), end="")
