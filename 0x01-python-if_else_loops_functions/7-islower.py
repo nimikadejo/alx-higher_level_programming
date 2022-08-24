c462db5ff76758ea01cc2d8f4d1d96a2f49090f4
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-24 12:40:22 -0700

    Changed int function to ord to correct checker errors

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def islower(c):
-    int_c = int(c)
+    int_c = ord(ci)
 
 
-if (int_c >= 97) and (int_c <= 122):
+if (int_c >= 97) and (int_c <= 123):
     return True
 else:
     return False
