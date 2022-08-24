cd26b67398208113149ee310039bf8bada602351
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-24 13:26:25 -0700

    task on converting input to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-
+    for word in str:
+        iterator = ord(str)
+    if (iterator >= 97) and (iterator <= 122):
+        converter = chr(iterator - 32)
+    elif (iterator >= 65) and (iterator <= 90):
+        converter = chr(iterator)
+    print("{}".format(converter))
+    else:
+        print("{}".format(iterator))
