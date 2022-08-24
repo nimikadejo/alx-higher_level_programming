9e16dcfa03f44ae284db92025c8520944b735961
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-24 12:44:28 -0700

    typo correction 2

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     int_c = ord(c)
-
-
-if (int_c >= 97) and (int_c <= 123):
-    return True
-else:
-    return False
+    if (int_c >= 97) and (int_c <= 123):
+        return True
+    else:
+        return False
