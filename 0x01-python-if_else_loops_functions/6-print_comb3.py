41a8608465d6869bee2c236d8efe0bf40ec71db9
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-24 11:25:14 -0700

    printing combinations under specific conditions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+for i in range(90):
+    if ((i % 10) > (i / 10)) and (i != 89):
+        print("{:02d}, ".format(i), end="")
+    elif (i == 89):
+        print("{:02d}".format(i))
