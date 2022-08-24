49ed887167458daf8112aa4b109469c1987a4b02
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-24 15:25:05 -0700

    introduced another variable

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
+    converter = ''
     for word in str:
         iterator = ord(str)
-    if (iterator >= 97) and (iterator <= 122):
-        converter = chr(iterator - 32)
-    elif (iterator >= 65) and (iterator <= 90):
-        converter = chr(iterator)
-    print("{}".format(converter))
-    else:
-        print("{}".format(iterator))
+        if (iterator >= 97) and (iterator <= 122):
+            converter += chr(iterator - 32)
+        elif (iterator >= 65) and (iterator <= 90):
+            converter += chr(iterator)
+        else:
+            converter += chr(iterator)
+    print("{}".format(iterator))
