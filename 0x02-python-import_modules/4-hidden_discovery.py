1fd6c418296a6464352e6d6b006bccd0ce279f33
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-25 16:15:41 -0700

    task on using os module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    full_list = dir(hidden_4)
+    for i in range(1, len(full_list)):
+        if "__" != full_list[i][:2]:
+            print("{}".format(full_list[i]))
