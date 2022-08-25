abb3824c802f55cd200f2c4790f51cd09d84e06b
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-25 15:36:06 -0700

    using sys module for argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv)
+    if count <= 1:
+        print("0 arguments.")
+    elif count == 2:
+        print("{:d} argument:".format(count - 1))
+    else:
+        print("{:d} arguments:".format(count - 1))
+    for i in range(1, count):
+        print("{:d}: {}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv)
+    if count <= 1:
+        print("0")
+    else:
+        addition = 0
+        for i in range(1, count):
+            addition += int((sys.argv[i]))
+    print("{}".format(addition))
