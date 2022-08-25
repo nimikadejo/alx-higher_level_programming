5ae19a7eaca54ab1a491e850667b912cf0709edb
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-25 15:47:58 -0700

    edit code to change 0 from str to var value

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    count = len(sys.argv)
-    if count <= 1:
-        print("0")
-    else:
-        addition = 0
-        for i in range(1, count):
+    count = len(sys.argv) - 1
+    addition = 0
+    if count > 0:
+        for i in range(1, (count + 1)):
             addition += int((sys.argv[i]))
     print("{}".format(addition))
