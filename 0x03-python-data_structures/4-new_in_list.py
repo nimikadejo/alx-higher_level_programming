ddb2f038a30a7f2a369476b7a16cca3a5a70fea2
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-29 14:13:32 -0700

    removed isinstance

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = mylist[:]
-    if (not isinstance(idx, int)):
-        return my_list.copy()
     if idx < 0:
         return new_list
     if idx > len((my_list) - 1):
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-#    if not matrix:
-#        return None
+    if not matrix:
+        return None
     for vert in matrix:
         if len(vert) == 0:
             print()
-        for hori in range(len(vert)):
-            print("{:d}".format(vert[hori],
+        for i in range(len(vert)):
+            print("{:d}".format(vert[i],
                   end="" if colomn == len((vert) - 1) else " "))
-        print("")
