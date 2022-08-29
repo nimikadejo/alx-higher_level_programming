248f5bdd0645151e74f4b2c7579fb781ebb5986e
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-29 15:35:08 -0700

    add if not for task 4 error

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = mylist[:]
+    if not my_list:
+        return
+    new_list = my_list.copy()
     if idx < 0:
         return new_list
     if idx > len((my_list) - 1):
