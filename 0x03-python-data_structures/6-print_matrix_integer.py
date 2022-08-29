1256570b1d9b2a0e628bc00611cfbfd22b47d662
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-29 16:03:56 -0700

    final correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return None
+#    if not matrix:
+#       return None
     for vert in matrix:
-        if len(vert) == 0:
-            print()
-        else:
-            for i in range(len(vert)):
-                print("{:d}".format(vert[i],
-                      end="" if colomn == len((vert) - 1) else " "))
-            print("")
+        for i in range(len(vert)):
+            print("{:d}".format(vert[i],
+                  end="" if colomn == len((vert) - 1) else " "))
+        print("")
