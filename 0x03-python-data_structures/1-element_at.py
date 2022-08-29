246f2b8d1dbced404725419f0fa6ff5ea3e27827
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-29 11:28:10 -0700

    correcting task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,10 +2,10 @@
 def element_at(my_list, idx):
     if (not isinstance(idx, int)):
         return None
-    if idx < 0:
+    elif idx < 0:
         return None
     elif idx > (len(my_list) - 1):
         return None
-    elif (len(mylist)) == 0:
-        return None
+#    elif (len(mylist)) == 0:
+#        return None
     return my_list[idx]
