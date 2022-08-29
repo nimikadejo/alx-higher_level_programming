1e89524bd256116053826093c96bd1839c9a297b
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-29 15:55:52 -0700

    4 and 6 errors

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,8 +4,9 @@ def new_in_list(my_list, idx, element):
         return
     new_list = my_list.copy()
     if idx < 0:
-        return new_list
+        return my_list[:]
     if idx > len((my_list) - 1):
+        return my_list[:]
+    else:
+        new_list[idx] = element
         return new_list
-    new_list[idx] = element
-    return new_list
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,8 @@ def print_matrix_integer(matrix=[[]]):
     for vert in matrix:
         if len(vert) == 0:
             print()
-        for i in range(len(vert)):
-            print("{:d}".format(vert[i],
-                  end="" if colomn == len((vert) - 1) else " "))
+        else:
+            for i in range(len(vert)):
+                print("{:d}".format(vert[i],
+                      end="" if colomn == len((vert) - 1) else " "))
+            print("")
