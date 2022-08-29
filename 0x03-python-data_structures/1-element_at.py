fc4a5be66c46b571a91b6429cf98847568033ef8
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-29 11:22:38 -0700

    added isinstance

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    if (not isinstance(idx, int)):
+        return None
     if idx < 0:
         return None
-    elif idx > (len(my_list)):
+    elif idx > (len(my_list) - 1):
         return None
     elif (len(mylist)) == 0:
         return None
-    else:
-        value = my_list[idx]
-        return value
+    return my_list[idx]
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+    if (not isinstance(idx, int)):
+        return 0
     if idx < 0:
         return my_list
-    elif idx > (len(my_list)):
+    elif idx > (len(my_list) - 1):
         return my_list
     elif (len(my_list)) == 0:
         return my_list
