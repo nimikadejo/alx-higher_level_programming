dedf622583dc6ed2cb2f697f7d022f3a2fef4757
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-31 14:36:46 -0700

    tried to correct 6 by adding a for loop

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     new_dict = items(sorted(a_dictionary))
-    print("{:s}: {}".format(new_dict.keys, new_dict.value))
+    for k, v in new_dict:
+        print("{:s}: {}".format(k, v))
