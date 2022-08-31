179f362ab2d9e81cd78f64ade776925947752809
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-31 16:28:23 -0700

    changed return

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,4 +4,4 @@ def multiply_by_2(a_dictionary):
     for key, value in a_dictionary.items():
         times2 = list(map(lambda value: value**2, a_dictionary))
         new_dict.append(times2)
-    return {new_dict.key: new_dict.value}
+    return {a_dictionary.key: new_dict.value}
