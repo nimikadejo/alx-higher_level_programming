a848f2aedf1e310cfd7de343ef02b4c010eca794
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-31 16:46:59 -0700

    pycode error fix

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-     new_dict = []
-    for key, value in a_dictionary.items():
-        times2 = list(map(lambda value: value*2, a_dictionary))
+    new_dict = []
+    for k, v in a_dictionary.items():
+        times2 = list(map(lambda v: v*2, a_dictionary))
         new_dict.append(times2)
     return {a_dictionary.key: new_dict.value}
