b49f0f17b6582f73c1396539f675ff74e2dcc169
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-31 14:42:43 -0700

    used for loop without item()

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    new_dict = items(sorted(a_dictionary))
-    for k, v in new_dict:
-        print("{:s}: {}".format(k, v))
+    new_dict = sorted(a_dictionary)
+    for item in new_dict:
+        print("{:s}: {}".format(item, new_dict[item]))
