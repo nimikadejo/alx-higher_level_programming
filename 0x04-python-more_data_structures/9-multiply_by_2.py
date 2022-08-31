d75159666c8d883f6726d78266fcf4f760b00268
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-31 16:24:39 -0700

    correction

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,5 +4,4 @@ def multiply_by_2(a_dictionary):
     for key, value in a_dictionary.items():
         times2 = list(map(lambda value: value**2, a_dictionary))
         new_dict.append(times2)
-        print("{}: {:d}".format(a_dictionary.key, new_dict.value))
-    return new_dict
+    return {new_dict.key: new_dict.value}
