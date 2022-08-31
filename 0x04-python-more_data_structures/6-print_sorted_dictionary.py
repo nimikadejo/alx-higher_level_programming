0a28356a81a2371931e46c684dc7b0591f6f2596
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-31 14:57:07 -0700

    removed new variable

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    new_dict = sorted(a_dictionary)
-    for item in new_dict:
-        print("{}: {}".format(item, new_dict[item]))
+    for item in sorted(a_dictionary):
+        print("{}: {}".format(item, a_dictionary[item]))
