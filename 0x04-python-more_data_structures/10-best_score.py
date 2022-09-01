b44d4ddc95fb6155d4c294e13530128cb011b4e2
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-31 17:31:33 -0700

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    biggest = 0
+    the_name = ''
+    if a_dictionary is None:
+        return None
+    for key in a_dictionary:
+        if a_dictionary[key] > biggest:
+            biggest = a_dictionary[key]
+            the_name = key
+    return (the_name)
