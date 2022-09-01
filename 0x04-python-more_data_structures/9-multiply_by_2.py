a5f05ac94d2e161d68e5ee794eff5ca14236170e
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-31 17:04:12 -0700

    map task

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = []
-    for k, v in a_dictionary.items():
-        times2 = list(map(lambda v: v*2, a_dictionary))
-        new_dict.append(times2)
-    return {a_dictionary.key: new_dict.value}
+    return {key: a_dictionary[key]*2 for key in a_dictionary}
