299793464af0df603617cfba46858d06c046c36c
nimikadejo <kadejotoluwanimi@gmail.com>
2022-09-12 15:17:40 -0700

    cupload

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     int_len = 0
-    for item in range(x):
+    for index in range(x):
         try:
-            print("{:d}".format(my_list[item]), end="")
+            print("{:d}".format(my_list[index]), end="")
             int_len += 1
         except IndexError:
             break
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,10 +2,10 @@
 def safe_print_division(a, b):
 
     try:
-        result = a / b
+        solution = a / b
     except ZeroDivisionError:
-        result = None
+        solution = None
     finally:
-        print("Inside result: {:d}".format(result))
+        print('Inside result: {}'.format(solution))
 
-        return result
+        return solution
