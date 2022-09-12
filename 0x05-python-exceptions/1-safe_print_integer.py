bdd8ad052a251c06567add9e15a1f7f13ffc4193
nimikadejo <kadejotoluwanimi@gmail.com>
2022-09-12 13:48:32 -0700

    fix pycodestyle error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
     except Exception as error:
-        print("{} is not an integer".format(value))
         return False
     else:
         return True
