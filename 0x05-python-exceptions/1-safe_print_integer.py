b07a25b272df65b31993341af49f5b9eb1d13b5f
nimikadejo <kadejotoluwanimi@gmail.com>
2022-09-12 13:22:53 -0700

    printing ints

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-def safe_print_integer???(my_list=[], x=0):
+def safe_print_integer(value):
     try:
+        new_value = isinstance(int(value))
 
+    except Exception as error:
+        print("{} is not an integer".format(value))
+        return False
+    else:
+        print("{} is an integer".format(value))
+        return True
