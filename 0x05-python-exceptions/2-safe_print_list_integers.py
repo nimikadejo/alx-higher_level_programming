d6cbcc720c647bba722cfb0600186da07da2a343
nimikadejo <kadejotoluwanimi@gmail.com>
2022-09-12 15:32:14 -0700

    raising errors

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     int_len = 0
-    for index in range(x):
+    for item in range(x):
         try:
-            print("{:d}".format(my_list[index]), end="")
+            print('{:d}'.format(my_list[item]), end='')
             int_len += 1
         except IndexError:
             break
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for index in range(list_length):
+        new_item = 0
+        try:
+            new_item = my_list_1[index] / my_list_2[index]
+        except ZeroDivisionError:
+            print('division by 0')
+        except TypeError:
+            print('wrong type')
+        except IndexError:
+            print('out of range')
+        finally:
+            new_list.append(new_item)
+
+    return new_list
