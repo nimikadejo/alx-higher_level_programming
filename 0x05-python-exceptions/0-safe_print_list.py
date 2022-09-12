2e8139e71908f5ec70136ffc250c454bc298c0f7
nimikadejo <kadejotoluwanimi@gmail.com>
2022-09-12 13:01:29 -0700

    chane end= to end=''

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     length = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
+            print(my_list[i], end='')
             length+= 1
         except Exception as Err:
             break
-    print("")
+    print('')
     return length
