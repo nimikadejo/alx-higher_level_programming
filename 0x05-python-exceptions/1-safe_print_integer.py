3cffa08ba1387b6ef1377bc74c85b091fc3f9d48
nimikadejo <kadejotoluwanimi@gmail.com>
2022-09-12 13:28:32 -0700

    remove isinstsnce

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        new_value = isinstance(int(value))
-
+        print("{:d}".format(value))
     except Exception as error:
         print("{} is not an integer".format(value))
         return False
     else:
-        print("{:d}".format(value))
         return True
