9e95b9caadbc1f163f0992626c23b0ced18b62e6
nimikadejo <kadejotoluwanimi@gmail.com>
2022-09-13 14:56:13 -0700

    final

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -19,13 +19,12 @@ class Square:
     @size.setter
     def size(self, value):
         """setting conditions to control field data"""
-        if type(value) is int:
-            if value >= 0:
-                self.__size = value
-            else:
-                raise ValueError("size must be >= 0")
-        else:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     @property
     def position(self):
