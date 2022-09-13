ad3e177f16e10aed8f9779232cea1d906482c6ae
nimikadejo <kadejotoluwanimi@gmail.com>
2022-09-13 11:18:47 -0700

    classes and objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A class Square that defines a square by size"""
+
+
+class Square:
+    """class definition"""
+    def __init__(self, size):
+        """initialization of class
+        args1: the class itself
+        args2: size variable of class
+        """
+        self.__size = size
+        """instantiation of self with size"""
