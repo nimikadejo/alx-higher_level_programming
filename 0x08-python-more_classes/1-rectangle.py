bcc013f9d1dcc2cb053efaa4fb1ec6c7b1fcdaab
nimikadejo <kadejotoluwanimi@gmail.com>
2022-09-19 13:39:29 -0700

    more classes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+This module defines the a Rectangle Object.
+"""
+
+
+class Rectangle:
+    """ Rectangle Class """
+    def __init__(self, width=0, height=0):
+        """ class initialization"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ private instantiation"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setting method for width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ private instantiation"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setting method for height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
