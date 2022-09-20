8544dbf482ecc47a6ec87098667f0d57edef1b4f
nimikadejo <kadejotoluwanimi@gmail.com>
2022-09-20 11:03:06 -0700

    say_my_name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    say_my_name: function that prints name in args
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ Prints 'My name is first_name last_name'
+    Args:
+    first_name (str): first name
+    last_name (str): last name
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print('My name is {} {}'.format(first_name, last_name))
+
+    if __name__ == "__main__":
+        import doctest
+        doctest.testfile("tests/3-say_my_name.txt")
