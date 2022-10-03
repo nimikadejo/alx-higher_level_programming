0c3105e7e3309a5e78a9a79912d7b61b4f88a03a
nimi.kadejo <kadejotoluwanimi@gmail.com>
2022-10-03 15:34:02 -0700

    test

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Unittests for base
+"""
+
+import unittest
+from models.base import Base
+
+
+def test_to_json_string_valid(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
