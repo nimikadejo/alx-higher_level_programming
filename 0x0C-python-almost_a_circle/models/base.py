16ba0579e9966fa1d60d99452fb3de25fa5ebc37
nimi.kadejo <kadejotoluwanimi@gmail.com>
2022-10-03 12:39:05 -0700

    class base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""this module contains base.py which implements the base class in this project
+"""
+import json
+import turtle
+
+
+class Base:
+    """ class definition"""
+
+    __nb_objects = 0
+
+
+    def __init__(self, id=None):
+        """class initialization"""
+        if self.id == None:
+            Base.__nb_objects += 1
+            self.id == Base.__nb_objects
+        else:
+            self.id == id
