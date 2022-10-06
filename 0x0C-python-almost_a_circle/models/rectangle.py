9319ca67259c3727d89670bd914b55e891086e79
nimi.kadejo <kadejotoluwanimi@gmail.com>
2022-10-06 09:16:38 -0700

    m

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,6 +100,6 @@ class Rectangle(Base):
     def display(self):
         """ replaces the size of rectangle with #"""
         for j in range(height):
-            print("#\n")
-        for i in range(width):
-            print("#", end="", sep='')
+            for i in range(width):
+                print("#", sep='', end='')
+            print('')
